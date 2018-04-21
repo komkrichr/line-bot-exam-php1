1e230fdceefd66bbd5512a6fb590fe8908765a06
komkrichr <ratchusiri@gmail.com>
2018-04-21 22:05:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['source']['userId'];
 			$msg_reply = $event['message']['text'];
-			if (strpos($msg_reply, 'lo/') !== false) {
+			if (strpos($msg_reply, 'lo/') !== false) || (strpos($msg_reply, 'Lo/') !== false) || (strpos($msg_reply, 'LO/') !== false) {
 				$msg_reply='send location';
 			}
 			//$msg_reply = 'ABC';
