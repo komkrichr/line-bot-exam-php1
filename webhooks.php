654c469ec45f0258ca3f01cd574ef3eb10fbe86f
komkrichr <ratchusiri@gmail.com>
2018-04-21 12:14:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,18 +17,19 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			//$text = $event['message'];
+			$_msg = $event['message'];
+			
 			//$text = 'Test';
 			//$text = $arrJson['events'][0]['message']['text'];
 			//if (strpos($text, 'Lo/') !== false) {
-			if ($text=='Lo') {
+			//if ($text=='Lo') {
 				// Get replyToken
 				$replyToken = $event['replyToken'];
 
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $text
+					'text' => $_msg
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
@@ -48,10 +49,9 @@ if (!is_null($events['events'])) {
 				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 				$result = curl_exec($ch);
 				curl_close($ch);
-			}
-			
+			//}
 			echo $result . "\r\n";
 		}
 	}
 }
-echo "OK1";
+echo "OK2";
