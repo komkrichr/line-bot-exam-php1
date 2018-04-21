0e44af48d0234936dd834ac28b7ca158ea767338
komkrichr <ratchusiri@gmail.com>
2018-04-22 06:17:18 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -68,8 +68,8 @@ $myvars = 'txtRaid=' . $msg_reply ;
 $ch = curl_init( $url );
 
 //$myvars =  curl_escape($ch ,'txtRaid=' . $msg_reply);
-curl_setopt( $ch, CURLOPT_ENCODING, 'UTF-8');
-//curl_setopt( $ch, CURLOPT_ENCODING, 'tis-620');
+//curl_setopt( $ch, CURLOPT_ENCODING, 'UTF-8');
+curl_setopt( $ch, CURLOPT_ENCODING, 'tis-620');
 curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
