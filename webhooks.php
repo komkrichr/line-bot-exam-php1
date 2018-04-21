dd74d6b6bc19d8149b9f05507dd752ad30ac4e04
komkrichr <ratchusiri@gmail.com>
2018-04-21 18:09:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $text
+					'text' => $_msg 
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
@@ -54,4 +54,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK2";
+echo "OK3";
