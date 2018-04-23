40f1e1fe309c56d6996d94319e737f9d553e7f14
komkrichr <ratchusiri@gmail.com>
2018-04-23 10:25:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $sMessage='Sพญาไทย ARLE';
 $iCount=strlen($sMessage);
 
 echo "$sMessage <br>";
-for ($x = 0; $x < $iCount); $x++) {
+for ($x = 0; $x < $iCount; $x++) {
     echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
     echo substr("000".ord(substr($sMessage,$x,1)),-3,3). "<br>";
 } 
