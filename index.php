f5b2e78a4ef33cf2d52fc8553aeca6d4fec35db9
komkrichr <ratchusiri@gmail.com>
2018-04-23 13:05:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
-<html>
-<head>
-<title>utf-8</title>
-<meta http-equiv=Content-Type content="text/html; charset="utf-8">
-</head>
-<body>
-
 <?php
-//mb_internal_encoding("UTF-8");
-//mb_http_output('UTF-8');
-//header('Content-type: text/html; charset=utf-8');
-
 echo "Hello LINE BOT V5. <br>";
 
 $sMessage='ทดสอบ ARL';
 $iCount=strlen($sMessage)-1;
 $AscMessage="";
                                                                  
-                                                                 
-echo $sMessage <br>";
-echo $x.": ". substr($sMessage,0,3) ."<br>";
-echo $x.": ". substr($sMessage,3,3) ."<br>";
-echo ord(substr($sMessage,0,3)) . "<br>";
 
 //echo mb_strlen($sMessage) . "<br>";                                               
                                                                  
@@ -34,7 +18,6 @@ for ($x = 0; $x <= $iCount; $x++) {
     //echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
     //$AscMessage.=substr("000".ord(substr($sMessage,$x,1)),-3,3);
     //echo substr("000".ord(substr($sMessage,$x,1)),-3,3). "<br>";
-
 }
 echo $AscMessage;
 
@@ -53,7 +36,3 @@ curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 //$response = curl_exec( $ch );
 ?>
-    
-</body>
-</html>
-
