f3d4c4623e4586441a41736cc8ff9bb674d47794
komkrichr <ratchusiri@gmail.com>
2018-04-23 09:59:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ echo "Hello LINE BOT";
 
 $sMessage='พญาไทย ARL';
 $iCount=strlen($sMessage);
- 
+
+echo "$sMessage <br>";
 for ($x = 0; $x <= $iCount; $x++) {
-    echo "The number is: $x <br>";
+    echo "The number is: $x substr($sMessage,$x,1) <br>";
 } 
 ?>
