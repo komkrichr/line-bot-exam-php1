358b16240c8012c289bafd65400488fb9873fdad
komkrichr <ratchusiri@gmail.com>
2018-04-23 10:02:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ $iCount=strlen($sMessage);
 
 echo "$sMessage <br>";
 for ($x = 0; $x <= $iCount; $x++) {
-    echo "The number is: $x " . substr($sMessage,$x,1) . "<br>";
+    echo "The number is: $x string: " . substr($sMessage,$x,1) . "<br>";
 } 
 ?>
