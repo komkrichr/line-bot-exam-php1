fc19a7675dbaa099d2dcf5336a73c4fad97e234e
komkrichr <ratchusiri@gmail.com>
2018-04-23 13:07:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello LINE BOT V5. <br>";
+echo "Hello LINE BOT V6. <br>";
 
 $sMessage='ทดสอบ ARL';
 $iCount=strlen($sMessage)-1;
@@ -9,12 +9,12 @@ $AscMessage="";
 //echo mb_strlen($sMessage) . "<br>";                                               
                                                                  
 for ($x = 0; $x <= $iCount; $x++) {
-    //if  ((ord(ord(substr($sMessage,$x,1)) <40) && (ord(ord(substr($sMessage,$x,1)) >95)) {   
+    if ((ord(substr($sMessage,$x,1)) <40) && (ord(substr($sMessage,$x,1)) >95)) {   
     //     echo $x.": ". substr($sMessage,$x,3) ."<br>";
     //     $x=$x+3;
     //}else{
     //    echo $x.": ". substr($sMessage,$x,1) ."<br>";
-    //}
+    }
     //echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
     //$AscMessage.=substr("000".ord(substr($sMessage,$x,1)),-3,3);
     //echo substr("000".ord(substr($sMessage,$x,1)),-3,3). "<br>";
