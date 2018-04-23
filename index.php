cd5fca2d99e96c394844f5c816ae4b7d052d59b8
komkrichr <ratchusiri@gmail.com>
2018-04-23 10:23:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-echo "Hello LINE BOT";
+echo "Hello LINE BOT V1. <br>";
 
-$sMessage='พญาไทย ARL';
+$sMessage='Sพญาไทย ARLE';
 $iCount=strlen($sMessage);
 
 echo "$sMessage <br>";
 for ($x = 0; $x <= $iCount; $x++) {
     echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
+    echo substr("000".ord(substr($sMessage,$x,1)),-3,3). "<br>";
 } 
 ?>
