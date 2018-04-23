59ed38f451cc93dfaf323fd9493b1b76e318000c
komkrichr <ratchusiri@gmail.com>
2018-04-23 12:11:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $iCount=strlen($sMessage)-1;
 $AscMessage="";
 echo "$sMessage <br>";
 
-for ($x = 0; $x <= 200; $x++) {
+for ($x = 0; $x <= $iCount-1; $x++) {
     //echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
     //$AscMessage.=substr("000".ord(substr($sMessage,$x,1)),-3,3);
     //echo substr("000".ord(substr($sMessage,$x,1)),-3,3). "<br>";
