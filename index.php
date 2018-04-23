837fc51589f07c44185acfcdbf059c3a5a8cf62c
komkrichr <ratchusiri@gmail.com>
2018-04-23 13:11:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ for ($x = 0; $x <= $iCount; $x++) {
          echo $x.": ". substr($sMessage,$x,3) ."<br>";
          $x=$x+3;
     }else{
-    //    echo $x.": ". substr($sMessage,$x,1) ."<br>";
+        echo $x.": ". substr($sMessage,$x,1) ."<br>";
     }
     //echo "The number is: $x string: " . substr($sMessage,$x,1) . " Asc:" . ord(substr($sMessage,$x,1)) . "<br>";
     //$AscMessage.=substr("000".ord(substr($sMessage,$x,1)),-3,3);
