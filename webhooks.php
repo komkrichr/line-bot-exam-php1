ca8842a9cb3c7a4c8e97fe68eb9dc3890dddc2fc
komkrichr <ratchusiri@gmail.com>
2018-08-07 03:25:56 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -31,7 +31,7 @@ curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
 curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 $jsonData = json_decode(curl_exec($curlSession));
 curl_close($curlSession);
-$msg = explode(' ',$jsonData->{'Raids'});
+//$msg = explode(' ',$jsonData->{'Raids'});
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -330,4 +330,4 @@ if (!is_null($events['events'])) {
 	}
 }
 
-echo "106";
+echo "107";
