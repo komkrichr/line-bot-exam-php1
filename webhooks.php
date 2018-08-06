624a3333598b8bff38596fca057c53dc6aa5923e
komkrichr <ratchusiri@gmail.com>
2018-08-07 00:28:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -48,6 +48,34 @@ if (!is_null($events['events'])) {
 				$AscMessage.=ordutf8($Message1, $offset)."|";
 			}
 
+			if (strpos($msg_reply, 'Raids') !== false) {
+				// Build message to reply back
+				$raids_message='goBot Location';
+				$replyToken = $event['replyToken'];
+				$messages = [
+					'type' => 'text',
+					'text' => $raids_message
+				];
+				
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+					$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				//$result = curl_exec($ch);
+				curl_close($ch);				
+			}
+			
 			if (strpos($msg_reply, '/Cancel') !== false) {
 				$url = 'http://43.254.133.192/raid/delete.asp';
 				$ch = curl_init( $url );
