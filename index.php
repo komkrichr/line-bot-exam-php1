9330c28c3f8fcfe23565d69998e2ea0cb28243d2
komkrichr <ratchusiri@gmail.com>
2018-08-07 10:02:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
-echo "Hello LINE BOT 2. <br>";
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
+function ordutf8($string, &$offset) {
+    $code = ord(substr($string, $offset,1)); 
+    if ($code >= 128) {        //otherwise 0xxxxxxx
+        if ($code < 224) $bytesnumber = 2;                //110xxxxx
+        else if ($code < 240) $bytesnumber = 3;        //1110xxxx
+        else if ($code < 248) $bytesnumber = 4;    //11110xxx
+        $codetemp = $code - 192 - ($bytesnumber > 2 ? 32 : 0) - ($bytesnumber > 3 ? 16 : 0);
+        for ($i = 2; $i <= $bytesnumber; $i++) {
+            $offset ++;
+            $code2 = ord(substr($string, $offset, 1)) - 128;        //10xxxxxx
+            $codetemp = $codetemp*64 + $code2;
+        }
+        $code = $codetemp;
+    }
+    $offset += 1;
+    if ($offset >= strlen($string)) $offset = -1;
+    return $code;
+}
+$access_token = 'MFFmuhxVsj5vCPrHZ7i2dKBNsg0ksdE36S2OuxYEEuFQ2zN5Uc2sQpsYD6/L5y201CM/h1AxwrVjrKItQc0kpqXjnRyieff+4iIKR+XSglNKGAIZZ9pb9jRyMyweJ/KxfNom4LHazjILRJXRiNyQBwdB04t89/1O/w1cDnyilFU=';
+$msg_reply='';
+// Get POST body content
+$content = file_get_contents('php://input');
+
+echo "Hello LINE BOT 1. <br>";
 
 $curlSession = curl_init();
 curl_setopt($curlSession, CURLOPT_URL, 'http://43.254.133.192/raid/botgo.asp');
@@ -11,4 +37,6 @@ echo $jsonData->{'Raids'};
 echo '<br>';
 $msg = iconv('ASCII', 'UTF-8//IGNORE', $jsonData->{'Raids'});
 echo $msg;
+
+echo 'stop';
 ?>
