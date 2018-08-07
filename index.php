d09bc26ed93119b4e51bd0d517c78bb8057df427
komkrichr <ratchusiri@gmail.com>
2018-08-07 09:55:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 echo "Hello LINE BOT 10. <br>";
+
+$curlSession = curl_init();
+curl_setopt($curlSession, CURLOPT_URL, 'http://43.254.133.192/raid/botgo.asp');
+curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+$jsonData = json_decode(curl_exec($curlSession));
+curl_close($curlSession);
+$msg = iconv('ASCII', 'UTF-8//IGNORE', $jsonData->{'Raids'});
+echo $msg;
 ?>
