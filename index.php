6d0153e84442db94ea9d92266beca8c9ace7f5c9
komkrichr <ratchusiri@gmail.com>
2018-10-08 13:51:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$url = 'http://43.254.133.192/raid/ar1.asp';
-//$url = 'https://www.settrade.com/settrade/#/home';
+//$url = 'http://43.254.133.192/raid/ar1.asp';
+$url = 'https://www.settrade.com/settrade/#/home';
 
 $AscMessage='Wannasorn/11.30';
 $myvars = 'txtRaid=' . $AscMessage  ;
@@ -8,8 +8,8 @@ $myvars = 'txtRaid=' . $AscMessage  ;
 $ch = curl_init( $url );
 
 $headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874']
-//curl_setopt( $ch, CURLOPT_VERBOSE, true);
-//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt( $ch, CURLOPT_VERBOSE, true);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt( $ch, CURLOPT_HEADER, $headers);
 curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
@@ -20,5 +20,5 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec( $ch );
 //echo $response . "\r\n";				
 
-echo 'GoRaid V9';
+echo 'GoRaid V4';
 ?>
