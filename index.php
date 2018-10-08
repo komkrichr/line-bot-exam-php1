769fcab86636f890816ad31cc726d390d5823543
komkrichr <ratchusiri@gmail.com>
2018-10-08 13:56:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 //$url = 'http://43.254.133.192/raid/ar1.asp';
 
-$AscMessage='Wannasorn/11.30';
-$myvars = 'txtRaid=' . $AscMessage  ;
+//$AscMessage='Wannasorn/11.30';
+//$myvars = 'txtRaid=' . $AscMessage  ;
 
-$ch = curl_init( $url );
+//$ch = curl_init( $url );
 
-$headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874']
+//$headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874']
 
-curl_setopt( $ch, CURLOPT_HEADER, $headers);
-curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);				
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt( $ch, CURLOPT_HEADER, $headers);
+//curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
+//curl_setopt( $ch, CURLOPT_POST, 1);
+//curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+//curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);				
+//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
 //$response = curl_exec( $ch );
 //echo $response . "\r\n";				
 
-echo 'GoRaid V4';
+echo 'GoRaid VG';
 ?>
