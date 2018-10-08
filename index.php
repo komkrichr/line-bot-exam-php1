ae1524879139f6aa1fdc578a150356e17e4ebe52
komkrichr <ratchusiri@gmail.com>
2018-10-08 14:09:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-$url = 'http://43.254.133.192/raid/ar1.asp';
+//$url = 'https://43.254.133.192/raid/ar1.asp';
 $url = 'https://pantip.com';
 
 $ch = curl_init( $url );
 $headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874'];
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt( $ch, CURLOPT_HEADER, $headers);
 curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
 curl_setopt( $ch, CURLOPT_POST, 1);
@@ -13,5 +14,5 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec( $ch );
 echo $url;
 echo '<br>';
-echo 'GoRaid VG1-3';
+echo 'GoRaid VG1-4';
 ?>
