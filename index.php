af39c47a830834793abf9e4e4d537fd2d09c87f4
komkrichr <ratchusiri@gmail.com>
2018-10-08 13:55:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 //$url = 'http://43.254.133.192/raid/ar1.asp';
-$url = 'https://www.settrade.com/settrade/#/home';
 
 $AscMessage='Wannasorn/11.30';
 $myvars = 'txtRaid=' . $AscMessage  ;
@@ -8,9 +7,7 @@ $myvars = 'txtRaid=' . $AscMessage  ;
 $ch = curl_init( $url );
 
 $headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874']
-//curl_setopt( $ch, CURLOPT_VERBOSE, true);
-//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
+
 curl_setopt( $ch, CURLOPT_HEADER, $headers);
 curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
 curl_setopt( $ch, CURLOPT_POST, 1);
@@ -20,6 +17,5 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 //$response = curl_exec( $ch );
 //echo $response . "\r\n";				
 
-echo $url ;
 echo 'GoRaid V4';
 ?>
