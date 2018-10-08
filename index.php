3cac225cecc52a313dc8378f372407d2ccdde151
komkrichr <ratchusiri@gmail.com>
2018-10-08 13:53:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,9 @@ curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);				
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec( $ch );
+//$response = curl_exec( $ch );
 //echo $response . "\r\n";				
 
+echo $url ;
 echo 'GoRaid V4';
 ?>
