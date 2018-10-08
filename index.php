be12978ad152492f368bfd1abc8e864011e1bf16
komkrichr <ratchusiri@gmail.com>
2018-10-08 13:51:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $myvars = 'txtRaid=' . $AscMessage  ;
 $ch = curl_init( $url );
 
 $headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874']
-curl_setopt( $ch, CURLOPT_VERBOSE, true);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt( $ch, CURLOPT_VERBOSE, true);
+//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt( $ch, CURLOPT_HEADER, $headers);
 curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
