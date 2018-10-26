1f18ff115ea54d87eb31d5e4167522dbe967e9c7
komkrichr <ratchusiri@gmail.com>
2018-10-26 09:40:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -327,7 +327,7 @@ if (!is_null($events['events'])) {
 					// Build message to reply back
 					$messages = [
 						'type' => 'text',
-						'text' => $msg_reply 
+						'text' => $replyToken.':'.$msg_reply 
 					];
 
 					//Save Location
