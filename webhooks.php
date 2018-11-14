8b74c5891a446b134ac0aef6f6f61c4f38fe6603
komkrichr <ratchusiri@gmail.com>
2018-11-14 13:34:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -106,7 +106,39 @@ if (!is_null($events['events'])) {
 				echo $result . "\r\n";
 			}
 
-			if ((strpos($msg_reply, 'IV100') !== false) || (strpos($msg_reply, '100IV') !== false) || (strpos($msg_reply, 'EX RAID GYM') !== false)) {
+			if ((strpos($msg_reply, 'Ex Raid Gym') !== false) || (strpos($msg_reply, 'EX RAID GYM') !== false)) {
+				$name =  $msg_reply;
+				$inputimage =  "";
+				$sTokenDev="sRCa53zmuuPNdZqvXtUYX2IxB24SlPpJd5V4VClZovv";			
+				$chOne = curl_init(); 
+				curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+				// SSL USE 
+				curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+				curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+				//POST 
+				curl_setopt( $chOne, CURLOPT_POST, 1); 
+				// Message 
+				curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name&imageThumbnail=$inputimage&imageFullsize=$inputimage"); 
+				//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name");   
+				// follow redirects 
+				curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+				//ADD header array 
+				$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sTokenDev.'', ); 
+				curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+				//RETURN 
+				curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+				$result = curl_exec( $chOne ); 
+				//Check error 
+				if(curl_error($chOne)) {
+				    echo 'error:' . curl_error($chOne); } 
+				else {
+				  $result_ = json_decode($result, true); 
+				  echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
+				} 
+				$msg_reply="";
+			}
+			
+			if ((strpos($msg_reply, 'IV100') !== false) || (strpos($msg_reply, '100IV') !== false)) {
 				$name =  $msg_reply;
 				$inputimage =  "";
 				$sTokenDev="sRCa53zmuuPNdZqvXtUYX2IxB24SlPpJd5V4VClZovv";			
