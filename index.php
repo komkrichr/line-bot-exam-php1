158ac016367c02d18eb8f957f46d41af58c4f26d
komkrichr <ratchusiri@gmail.com>
2018-11-14 13:10:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 
-$Data="Warp Driver: 🇺🇸 #100IV 💯 Turtwig CP1102 IV100.0 LVL35
-COORDINATES 🔽
-42.588611,-88.359865
-GOOGLE MAPS 🔽
-https://t.co/IUnQ2uQ37J";
-
-
+$Data="Warp Driver: 🇺🇸 #100IV 💯 Turtwig CP1102 IV100.0 LVL35 COORDINATES 🔽";
 $line = split("\\n", $Data);
 echo $line[0];
-echo $line[1];
-echo $line[3];
 ?>
