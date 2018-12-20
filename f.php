89420ac12b6dc45d5c3e2722466466e9ac3e52e7
komkrichr <ratchusiri@gmail.com>
2018-12-20 08:24:31 +0700

    Update f.php

--- f.php
+++ f.php
@@ -69,7 +69,7 @@ if ($_POST) {
   echo sizeof($adata);
   if (sizeof($adata)>1) {
     foreach($adata as $key => $val) {
-      $data = preg_replace(",0-9", "", $adata[$key]);
+      $data = preg_replace("/[^0-9.,]/", "", $adata[$key]);
       echo $data;
       if (strpos($data, ',') !== false) {
              echo $adata[$key]."<br><br>" ;
