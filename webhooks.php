a92cf4b6a3b5e76c29ebca42daf75bd90a7c9e5c
komkrichr <ratchusiri@gmail.com>
2019-02-18 09:34:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -106,7 +106,7 @@ if (!is_null($events['events'])) {
 				echo $result . "\r\n";
 			}
 
-			if ((strpos($msg_reply, 'Co-ords') !== false) || (strpos($msg_reply, 'Exraid') !== false) || (strpos($msg_reply, 'Ex Raid Gym') !== false) || (strpos($msg_reply, 'EX RAID GYM') !== false)) {
+			if ((strpos($msg_reply, 'Co-ords') !== false) || (strpos($msg_reply, 'Exraid') !== false) || (strpos($msg_reply, 'Ex Raid') !== false) || (strpos($msg_reply, 'EX RAID') !== false)) {
 				$name =  $msg_reply;
 				$inputimage =  "";
 				$sTokenDev="NEvWA3b8STpVWM3HOFUVwulGlzQPWnnw5m0xNlXw8MW";			
