fb0fa0524872c0e898cf8e12dbff5cd955c376ab
komkrichr <ratchusiri@gmail.com>
2019-11-27 17:07:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,29 +3,9 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-function ordutf8($string, &$offset) {
-    $code = ord(substr($string, $offset,1)); 
-    if ($code >= 128) {        //otherwise 0xxxxxxx
-        if ($code < 224) $bytesnumber = 2;                //110xxxxx
-        else if ($code < 240) $bytesnumber = 3;        //1110xxxx
-        else if ($code < 248) $bytesnumber = 4;    //11110xxx
-        $codetemp = $code - 192 - ($bytesnumber > 2 ? 32 : 0) - ($bytesnumber > 3 ? 16 : 0);
-        for ($i = 2; $i <= $bytesnumber; $i++) {
-            $offset ++;
-            $code2 = ord(substr($string, $offset, 1)) - 128;        //10xxxxxx
-            $codetemp = $codetemp*64 + $code2;
-        }
-        $code = $codetemp;
-    }
-    $offset += 1;
-    if ($offset >= strlen($string)) $offset = -1;
-    return $code;
-}
 
 $access_token = '69d91fafb082e44307f374646698b32b';
 $msg_reply='';
-
-
 
 // Get POST body content
 $content = file_get_contents('php://input');
