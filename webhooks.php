e67090b47d4c08a07cd154e7f6e00610b5d382e9
komkrichr <ratchusiri@gmail.com>
2019-11-27 17:55:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -4,6 +4,41 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 
+$Token = "NEvWA3b8STpVWM3HOFUVwulGlzQPWnnw5m0xNlXw8MW";
+$name =  "..";
+$inputimage =  "";
+
+date_default_timezone_set("Asia/Bangkok");
+//line Send
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name&imageThumbnail=$inputimage&imageFullsize=$inputimage"); 
+//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name");   
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$Token.'', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) {
+    echo 'error:' . curl_error($chOne); } 
+else {
+  $result_ = json_decode($result, true); 
+  echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
+} 
+//Close connect 
+curl_close( $chOne );      
+
+
 $access_token = '1653577809 69d91fafb082e44307f374646698b32b';
 $msg_reply='';
 
@@ -54,4 +89,4 @@ if (!is_null($events['events'])) {
 	}
 }
 
-echo "<br>Fresh me 4";
+echo "<br>Fresh me 5";
