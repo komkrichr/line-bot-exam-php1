a72c50511eaa02f359d55b6fa183ea1dda88337e
komkrichr <ratchusiri@gmail.com>
2019-11-27 17:26:35 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '69d91fafb082e44307f374646698b32b';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
