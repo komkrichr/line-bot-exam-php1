00d29d98e381cd259a6fee5ef14381944b96f5dd
komkrichr <ratchusiri@gmail.com>
2019-11-27 17:49:03 +0700

    Update notify.php

--- notify.php
+++ notify.php
@@ -67,12 +67,9 @@ if ($_POST) {
 //Setting
 $lineapi = "UjHSi0jzuaGgX7kLvlbuBYoMzbo98eiD9doYdhTeb2F";
 
-$Token = "K6R3pKOXUxu4eh84eivsUTZRZL6lDzt7n8LvB8x88Uv";
-$Token1 = "IRBcmOtiPol9awe67vgeNpOupkfcDUmLCGsEXn0TdWK" ;
 
 $Token = "UjHSi0jzuaGgX7kLvlbuBYoMzbo98eiD9doYdhTeb2F";
-$Token1 = "xNxsyQNaXSeI59vk5cK5Ui2U28zQuw51DFDMDq55Hk8" ;  
-  
+ 
     
 $name =  trim($_POST['textArea']);
 $inputimage =  trim($_POST['inputimage']);
@@ -127,34 +124,7 @@ else {
   $result_ = json_decode($result, true); 
   echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
 } 
-
-$chOne = curl_init(); 
-curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
-// SSL USE 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
-//POST 
-curl_setopt( $chOne, CURLOPT_POST, 1); 
-// Message 
-curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name&imageThumbnail=$inputimage&imageFullsize=$inputimage"); 
-//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name");   
-// follow redirects 
-curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
-//ADD header array 
-$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$Token1.'', ); 
-curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
-//RETURN 
-curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-$result = curl_exec( $chOne ); 
-
-//Check error 
-if(curl_error($chOne)) {
-    echo 'error:' . curl_error($chOne); } 
-else {
-  $result_ = json_decode($result, true); 
-  echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
-}   
-  
+ 
 //Close connect 
   
 curl_close( $chOne );      
