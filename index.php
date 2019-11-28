a108ec2967f515aaf301a808ab7b762e70e1c071
komkrichr <ratchusiri@gmail.com>
2019-11-28 17:09:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Fresh me";
+
 
 $url = parse_url(getenv("mysql://b4eebb1ab31fba:8b0430ea@us-cdbr-iron-east-05.cleardb.net/heroku_a797b8e9f9df240?reconnect=true"));
 $server = $url["host"];
@@ -8,8 +8,9 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-echo $server . "<br>";
-echo $username . "<br>";
-echo $password . "<br>";
-echo $db . "<br>";
+echo "srv:". $server . "<br>";
+echo "usr:".$username . "<br>";
+echo "pw:".$password . "<br>";
+echo "db:".$db . "<br>";
+echo "Clear DB";
 ?>
