042135f863fad75dafd704ba313c8e675b4d27b4
komkrichr <ratchusiri@gmail.com>
2019-11-28 09:23:59 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,66 +1,48 @@
-<?php // callback.php
+<?php
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+require_once('./LINEBotTiny.php');
+$channelAccessToken = '69d91fafb082e44307f374646698b32b';
+$channelSecret = '1653577809';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
 
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-	
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('69d91fafb082e44307f374646698b32b');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1653577809']);
-
-$Token = "hmFgqgYqcLfSEcJwwTQXXqPHeAa8wQqTkM99zI83Owk";
-$name =  "..";
-$inputimage =  "";
-date_default_timezone_set("Asia/Bangkok");
-//line Send
-$chOne = curl_init(); 
-curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
-// SSL USE 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
-//POST 
-curl_setopt( $chOne, CURLOPT_POST, 1); 
-// Message 
-curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name&imageThumbnail=$inputimage&imageFullsize=$inputimage"); 
-//curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$name");   
-// follow redirects 
-curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
-//ADD header array 
-$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$Token.'', ); 
-curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
-//RETURN 
-curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-$result = curl_exec( $chOne ); 
-//Check error 
-if(curl_error($chOne)) {
-    echo 'error:' . curl_error($chOne); } 
-else {
-  $result_ = json_decode($result, true); 
-  echo "status : ".$result_['status']; echo "message : ". $result_['message']; 
-} 
-//Close connect 
-curl_close( $chOne );      
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			$response = $bot->replyText($replyToken, 'hello!');
-			
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-			if ($response->isSucceeded()) {
-			    echo 'Succeeded!';
-			    return;
-			}
-		}
-	}
-}
-
-echo "<br>Fresh me 8";
+echo "<br>LINEBotTiny";
