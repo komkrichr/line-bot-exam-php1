dcf0cb1e5addb9f65465b4f4aeecb6d959683c56
komkrichr <ratchusiri@gmail.com>
2019-11-28 18:01:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $url = parse_url(getenv("mysql://b4eebb1ab31fba:8b0430ea@us-cdbr-iron-east-05.cl
 $server = "us-cdbr-iron-east-05.cleardb.net";
 $username = "b4eebb1ab31fba";
 $password = "8b0430ea";
-$db = "heroku_a797b8e9f9df240";
+$db = "mydb";
 
 $conn = new mysqli($server, $username, $password, $db);
 // Check connection
