518bdcbc622a94b0c53caf47b3e61f1e535713b9
komkrichr <ratchusiri@gmail.com>
2019-11-28 13:02:40 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -65,7 +65,35 @@ if (!is_null($events['events'])) {
             curl_close($ch);
             echo $result . "";
         }
+        
+        if ($event['type'] == 'beacon') {
+            $replyToken = $event['replyToken'];
+            $text ='ซื้อ Dark Coco Brown Sugar ได้ในราคา 1 บาท ผ่าน Application Klook';
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text,
+            ];
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages]
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result . "";            
+        }
+       
     }
 }
 
-echo "<br>0011";
+echo "<br>0012";
