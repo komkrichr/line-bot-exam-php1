86c534ab82176bedf83673636eafc58b1a9c9021
komkrichr <ratchusiri@gmail.com>
2019-11-28 10:28:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,37 +1,6 @@
 <?php
 
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$channelAccessToken = '1653577809';
-$channelSecret = '69d91fafb082e44307f374646698b32b';
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                                'type' => 'text',
-                                'text' => $message['text']
-                            ]
-                        ]
-                    ]);
-                    break;
-                default:
-                    error_log('Unsupported message type: ' . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log('Unsupported event type: ' . $event['type']);
-            break;
-    }
-};
 
 echo "<br>LINEBotTiny-1";
 ?>
