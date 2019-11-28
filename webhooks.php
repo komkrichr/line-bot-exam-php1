646372c51719ec70430e506c2fe5821f2dcc1d52
komkrichr <ratchusiri@gmail.com>
2019-11-28 15:22:40 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -276,7 +276,8 @@ if (!is_null($events['events'])) {
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => [$messages]
+                'messages' => [$jsonFlex1]    
+                //'messages' => [$messages]
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
