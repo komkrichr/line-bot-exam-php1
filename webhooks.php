27f60eda0d1df553cc12819e3700d760f1f17c15
komkrichr <ratchusiri@gmail.com>
2019-11-28 10:30:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,7 +5,8 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $channelAccessToken = '1653577809';
 $channelSecret = '69d91fafb082e44307f374646698b32b';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
 
-echo "<br>LINEBotTiny-1";
+echo "<br>LINEBotTiny-2";
 ?>
