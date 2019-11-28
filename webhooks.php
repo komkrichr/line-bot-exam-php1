5469a30cdcfc83fe7907fb1a7f026dda59e4a879
komkrichr <ratchusiri@gmail.com>
2019-11-28 09:15:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -6,7 +6,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('69d91fafb082e44307f374646698b32b');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1653577809']);
 
-$Token = "NEvWA3b8STpVWM3HOFUVwulGlzQPWnnw5m0xNlXw8MW";
+$Token = "hmFgqgYqcLfSEcJwwTQXXqPHeAa8wQqTkM99zI83Owk";
 $name =  "..";
 $inputimage =  "";
 date_default_timezone_set("Asia/Bangkok");
@@ -39,12 +39,6 @@ else {
 //Close connect 
 curl_close( $chOne );      
 
-
-$access_token = '1653577809 69d91fafb082e44307f374646698b32b';
-$msg_reply='';
-
-file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -57,37 +51,16 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $msg_reply 
-			];
+			$response = $bot->replyText($replyToken, 'hello!');
 			
-			// Get text sent
-			$text = $event['source']['userId'];
-			$msg_reply = $event['message']['text'];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//$result = curl_exec($ch);
-			curl_close($ch);
-			//echo $result . "\r\n";
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+			if ($response->isSucceeded()) {
+			    echo 'Succeeded!';
+			    return;
+			}
 		}
 	}
 }
 
-echo "<br>Fresh me 7";
+echo "<br>Fresh me 8";
