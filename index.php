0847de92f08e2f4184393744a4d2e6a06d8b8356
komkrichr <ratchusiri@gmail.com>
2019-11-28 17:52:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,5 +12,14 @@ $conn = new mysqli($server, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$sql = "insert into user(line_id,user_name) values('xxxxx','xxxxxx')";
+if ($conn->query($sql) === TRUE) {
+    echo "Insert successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
 echo "Connected successfully"
 ?>
