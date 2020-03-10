466aeb6d35a3b80afe28fac12ab54d5078b71641
komkrichr <ratchusiri@gmail.com>
2020-03-10 12:54:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,17 +13,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error."<br>");
 }
     
-$sql = "CREATE TABLE MyGuests (line_id varchar(100) not null,user_name varchar(200)) ";
-if ($conn->query($sql) === TRUE) {
-}
+//$sql = "CREATE TABLE MyGuests (line_id varchar(100) not null,user_name varchar(200)) ";
+//if ($conn->query($sql) === TRUE) {
+//}
 
-$sql = "insert into MyGuests(line_id,user_name) values('xxxxx','xxxxxx')";
-if ($conn->query($sql) === TRUE) {
-    echo "Insert successfully";
-} else {
-    echo "Error : " . $conn->error ."<br>";
-}
-
+//$sql = "insert into MyGuests(line_id,user_name) values('xxxxx','xxxxxx')";
+//if ($conn->query($sql) === TRUE) {
+//    echo "Insert successfully";
+//} else {
+//    echo "Error : " . $conn->error ."<br>";
+//}
+    //*** PROMOTION SEND AND KEEP LOG DB ***//
+    $sql = "SELECT * FROM product_price ";
+    $result = $conn->query($sql);
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        printf("Product ", $row["product_id"], $row["product_price_id"]);
+    }
 $conn->close();
 
 echo "Connected successfully"."<br>";
