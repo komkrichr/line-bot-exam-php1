bf674e05843b9136938ade30afbfd90ae0a601aa
komkrichr <ratchusiri@gmail.com>
2020-05-18 13:47:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -129,11 +129,8 @@ $id=1;
 $sql = "SELECT max(redream_id) as 'max_id'  FROM redreams";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-	//while($row = $result->fetch_assoc()) {
-		$row = $result->fetch_assoc();
-		echo $row["max_id"];
-		$id = $row["max_id"];+1;
-	//}
+	$row = $result->fetch_assoc();
+	$id = $row["max_id"]+1;
 }
 $result -> free_result();
 $conn->close();	
