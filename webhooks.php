f8ac3a21b752eff48a9769f20f6d708c85b2f301
komkrichr <ratchusiri@gmail.com>
2020-05-18 15:47:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -201,7 +201,38 @@ if (!is_null($events['events'])) {
             } else {
                 SendLineNotify("Error : " . $conn->error);
             }
+            
+            if ($event['beacon']['type'] =='enter')
+            {
+                $text = $text.'Check-in';
+            }else if ($event['beacon']['type'] =='leave'){
+                $text = $text.'Check-out';    
+            }
 
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text,
+            ];
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$jsonSpecialOffer]    
+                //'messages' => [$messages]
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result . "";
+            
         }       
     }
 }
