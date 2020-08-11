ea2c4bd6f87b3c686d2f86a59c14737748d3bd96
komkrichr <ratchusiri@gmail.com>
2020-08-11 14:18:52 +0700

    Update go.php

--- go.php
+++ go.php
@@ -1,18 +1,3 @@
 <?php
-$url = 'http://43.254.133.192/raid/ar1.asp';
-//$url = 'https://pantip.com';
-$myvars = 'txtRaid=' . 'Wannasorn/15.00'  ;
-$ch = curl_init( $url );
-$headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'charset' => 'windows-874'];
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt( $ch, CURLOPT_HEADER, $headers);
-curl_setopt( $ch, CURLOPT_ENCODING, 'windows-874');
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);				
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec( $ch );
-echo $url;
-echo '<br>';
-echo 'GoRaid VG1-8';
+echo 'Go';
 ?>
