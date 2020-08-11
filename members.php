6820c27cbd71778d756a0657e9585a001718d401
komkrichr <ratchusiri@gmail.com>
2020-08-11 14:41:51 +0700

    Update members.php

--- members.php
+++ members.php
@@ -116,10 +116,10 @@ if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
 	    $count++;
 	    echo "<tr><td>".$count."</td>";
+  	    echo "<td>".$row["display_name"]."</td></tr>";	
 	    echo "<td>".$row["first_name"]." ".$row["first_name"]."</td>";		
-	    echo "<td>".$row["line_id"]."</td></tr>";	
+	    echo "<td>".$row["line_id"]."</td>";	
 	    echo "<td>".$row["hwid"]."</td></tr>";	
-	    echo "<td>".$row["display_name"]."</td></tr>";	
 	}
 	echo "</table>";
 	echo "</div>";
